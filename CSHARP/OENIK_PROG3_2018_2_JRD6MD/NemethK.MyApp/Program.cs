dda2e278e97a3b324a713ab24488a4d4bc4a90e0
Krisztián Németh <nkrisz5@gmail.com>
2018-10-15 17:24:41 +0200

    Added stylecop.json

--- CSHARP/OENIK_PROG3_2018_2_JRD6MD/NemethK.MyApp/Program.cs
+++ CSHARP/OENIK_PROG3_2018_2_JRD6MD/NemethK.MyApp/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Program.cs" company="JRD6MD">
+// Copyright (c) JRD6MD. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
 
 namespace NemethK.MyApp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     /// <summary>
     ///  Main class of the program
     /// </summary>
