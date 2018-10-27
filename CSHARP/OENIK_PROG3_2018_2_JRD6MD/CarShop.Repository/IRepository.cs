ec518190dad7f7ffd18ea7a0aa770664ecc3c183
Krisztián Németh <nkrisz5@gmail.com>
2018-10-28 00:57:58 +0200

    Added IRepository interface

---
+++ CSHARP/OENIK_PROG3_2018_2_JRD6MD/CarShop.Repository/IRepository.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="IRepository.cs" company="CarShop">
+// Copyright (c) CarShop. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace CarShop.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// IRepository Interface
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Create
+        /// </summary>
+        void Create();
+
+        /// <summary>
+        /// Read
+        /// </summary>
+        void Read();
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        void Delete();
+    }
+}
