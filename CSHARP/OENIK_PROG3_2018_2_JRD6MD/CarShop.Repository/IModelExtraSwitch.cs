c22a7e15a2bdc32904d45875d6ec7c08e18d5e40
Krisztián Németh <nkrisz5@gmail.com>
2018-11-07 10:50:12 +0100

    Created empty interface for ModelExtraSwitch.

---
+++ CSHARP/OENIK_PROG3_2018_2_JRD6MD/CarShop.Repository/IModelExtraSwitch.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IModelExtraSwitch.cs" company="CarShop">
+// Copyright (c) CarShop. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace CarShop.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CarShop.Data;
+
+    /// <summary>
+    /// ModelExtraSwitch specific functions
+    /// </summary>
+    public interface IModelExtraSwitch : IRepository<ModelExtraswitch>
+    {
+    }
+}
