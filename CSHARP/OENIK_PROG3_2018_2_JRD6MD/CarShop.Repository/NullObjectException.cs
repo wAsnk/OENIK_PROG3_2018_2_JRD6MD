8ba166d0eca8e2df670469b5bf6903c75000e42d
nkrisz5 <nkrisz5@gmail.com>
2018-11-30 18:34:45 +0100

    Created null object exception

---
+++ CSHARP/OENIK_PROG3_2018_2_JRD6MD/CarShop.Repository/NullObjectException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="NullObjectException.cs" company="CarShop">
+// Copyright (c) CarShop. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace CarShop.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Exception which is thrown in case of a null object.
+    /// </summary>
+    public class NullObjectException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NullObjectException"/> class.
+        /// </summary>
+        /// <param name="nullObject">The object with null in it</param>
+        /// <param name="message">Message for the exception.</param>
+        public NullObjectException(object nullObject, string message)
+        {
+            this.EMessage = message;
+            this.NullObject = nullObject;
+        }
+
+        /// <summary>
+        /// Gets or sets exception message
+        /// </summary>
+        public string EMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets exception nullobject
+        /// </summary>
+        public object NullObject { get; set; }
+    }
+}
